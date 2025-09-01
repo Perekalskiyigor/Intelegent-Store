491a28df455ef38b6e2fe5a10e2171215c4b126f
Igor Perekalskiy <46609959+Perekalskiyigor@users.noreply.github.com>
2025-09-01 10:49:25 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# Конфигурация подключения к базе данных PostgreSQL
+
+DB_CONFIG = {
+    "host": "172.21.10.182",
+    "port": 5432,
+    "dbname": "postgres",
+    "user": "postgres",
+    "password": "1"
+}
+
